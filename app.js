82e88b54b181169084c247bc8626c080f4b3fddc
Ciprian Cozma <cozma_ciprian19@yahoo.com>
2024-08-21 23:37:52 +0300

    added new files

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 // const { calculateSum } = require("./calculate/sum");
+// const { calculateMultiply } = require("./calculate/multiply")
+
 // import { calculateMultiply } from "./calculate/multiply.js";
 // import { calculateSum } from "./calculate/sum.js";
-// const { calculateMultiply } = require("./calculate/multiply")
+
 const { calculateMultiply, calculateSum } = require("./calculate");
+const data = require("./data.json");
+const { divide } = require("./iife");
 
 var name = "Namaste Node";
 var a = 10;
@@ -16,3 +20,6 @@ var b = 20;
 
 calculateSum(a, b);
 calculateMultiply(a, b);
+
+console.log(JSON.stringify(data));
+
---
+++ iife.js
@@ -0,0 +1,10 @@
+// IIFE
+// (function(module) {
+
+//     function divide(a, b) {
+//         const result = a - b;
+//         console.log(result)
+//     }
+
+//     module.exports = { divide }
+// })(module.exports = {})
