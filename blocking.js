629bdc8734e81a1f0fcfc1fa78ce5b244d4f2788
Ciprian Cozma <cozma_ciprian19@yahoo.com>
2024-08-31 23:12:41 +0300

    updated blocking file

--- blocking.js
+++ blocking.js
@@ -5,7 +5,12 @@ console.log("Hello again");
 var a = 1078698;
 var b = 20986;
 
-crypto.pbkdf2Sync("password", "salt", 500000, 50, "sha512", (err, data) => {
+// this one below will block the main thread, no callback is passed as it is for the one method without sync
+crypto.pbkdf2Sync("password", "salt", 500000, 50, "sha512");
+console.log("First key is generated!");
+
+// Password Base Key Derivation Function
+crypto.pbkdf2("password", "salt", 500000, 50, "sha512", (err, data) => {
     console.log("Key is generated!")
 })
 
